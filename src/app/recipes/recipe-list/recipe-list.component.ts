745e96172af90533fff781b4b8ab3e6f84e85cda
omarkusan <omarkusan@gmail.com>
2017-06-04 18:35:26 -0500

    added services and dependency injections

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit,EventEmitter,Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output()recipeWasSelected = new EventEmitter<Recipe>();
-  recipes:Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test','http://www.seriouseats.com/images/2013/05/050813-uyghur-251448-cw-chickenplate.JPG'),
-     new Recipe('Second Test Recipe','This is simply a test','http://1tv2o1g0gwh145y5v53ozm13-wpengine.netdna-ssl.com/wp-content/uploads/2014/01/Uyghur-Kebab.jpg'),
-    ];
-  constructor() { }
+
+  recipes:Recipe[];
+  constructor(private recipeService:RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
-  onRecipeSelected(recipe:Recipe){
-    this.recipeWasSelected.emit(recipe);
-  }
+  // onRecipeSelected(recipe:Recipe){
+  //   this.recipeWasSelected.emit(recipe);
+  // }
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,27 @@
+import { EventEmitter, Injectable} from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+@Injectable()
+export class RecipeService{
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes:Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test','http://www.seriouseats.com/images/2013/05/050813-uyghur-251448-cw-chickenplate.JPG',
+                [
+                    new Ingredient('Meat',1),
+                    new Ingredient('French Fries',20)
+                ]),
+     new Recipe('Second Test Recipe','This is simply a test','http://1tv2o1g0gwh145y5v53ozm13-wpengine.netdna-ssl.com/wp-content/uploads/2014/01/Uyghur-Kebab.jpg',
+                [
+                    new Ingredient('Buns',2),
+                    new Ingredient('Meat',1)
+                ]),
+    ];
+    constructor(private slService:ShoppingListService){}
+    getRecipes(){
+        return this.recipes.slice();    
+    }
+    addIngredientsToShoppingList(ingredient:Ingredient[]){
+        this.slService.addIngredients(ingredient);
+    }
+}
