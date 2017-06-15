65b115528745adc5ce2b6a8aa9f11533fc86abdb
omarkusan <omarkusan@gmail.com>
2017-06-15 12:11:23 -0500

    added form that can add, update, delete the items

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService{
     ingredientChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
     private ingredients:Ingredient[]= [
       new Ingredient('Apples',5),
       new Ingredient('Tomatoes',10),
@@ -12,8 +13,15 @@ export class ShoppingListService{
   getIngredients(){
       return this.ingredients.slice();
   }
+  getIngredient(index:number){
+    return this.ingredients[index];
+  }
   addIngredient(ingredient:Ingredient){
     this.ingredients.push(ingredient);
+    this.ingredientChanged.next(this.ingredients.slice());
+  }
+  updateIngredient(index:number,newIngredient:Ingredient){
+    this.ingredients[index] = newIngredient;
     this.ingredientChanged.next(this.ingredients.slice());
   }
   addIngredients(ingredients:Ingredient[]){
@@ -23,4 +31,8 @@ export class ShoppingListService{
     this.ingredients.push(...ingredients);
     this.ingredientChanged.next(this.ingredients.slice());
   }
+  deleteIngredient(index:number){
+    this.ingredients.splice(index,1);
+    this.ingredientChanged.next(this.ingredients.slice())
+}
 }
