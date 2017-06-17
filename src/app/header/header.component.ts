271fbb008cea7fe93c4c8030877e3cc2bd851dcd
omarkusan <omarkusan@gmail.com>
2017-06-17 18:22:03 -0500

    setting up Firebase as Backend, added post, get request functions

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,21 @@
 import {Component} from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
+import { Response } from '@angular/http';
 @Component({
     selector:'app-header',
     templateUrl:'./header.component.html',
 })
 export class HeaderComponent{
-    
+    constructor(private dataStorgeService:DataStorageService){}
+    onSaveData(){
+        this.dataStorgeService.storeRecipes()
+            .subscribe(
+                (response:Response) =>{
+                    console.log(response);
+                }
+            );
+    }
+    onFetchData(){
+        this.dataStorgeService.getRecipes();
+    }
 }
